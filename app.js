c9c1dfb3c182ac663e8d525e845eba2990a346a8
ceoyegetaneh <lijnati3@gmail.com>
2023-08-25 07:21:22 +0400

    Humburger nav and back to top are added

--- app.js
+++ app.js
@@ -1,2 +1,57 @@
-//let message = 'Welcome to our Digital Menu';
-//alert(message);
+
+document.addEventListener('DOMContentLoaded', function() {
+  const navLinks = document.querySelectorAll('div a');
+  
+  for (const link of navLinks) {
+    link.addEventListener('click', smoothScroll);
+  }
+
+  function smoothScroll(event) {
+    event.preventDefault();
+
+    const targetId = this.getAttribute('href');
+    const targetElement = document.querySelector(targetId);
+
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop,
+        behavior: 'smooth' // This enables smooth scrolling
+      });
+    }
+  }
+});
+document.addEventListener('DOMContentLoaded', function() {
+  const backToTopButton = document.getElementById('backToTopBtn');
+
+  backToTopButton.addEventListener('click', scrollToTop);
+
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+});
+
+// Humburger Menu
+function toggleMenu() {
+  var menu = document.getElementById("menu");
+  menu.classList.toggle("open");
+}
+
+//scroll to top
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
+
+window.addEventListener("scroll", function() {
+  var backToTopButton = document.querySelector(".back-to-top");
+  if (window.scrollY > 300) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+});
